0d4520bffc529df2570d47bd0b4bac58eb90567d
zjh <zoujh99@qq.com>
2020-06-24 11:56:42 +0800

    调整sdk_test.go

--- application/sdk_test.go
+++ application/sdk_test.go
@@ -4,16 +4,17 @@
  * @Date: 2020/3/4 5:14 下午
  * @Description: sdk的测试
  */
-package blockchain
+package main_test
 
 import (
 	"fmt"
+	"github.com/togettoyou/blockchain-real-estate/application/blockchain"
 	"testing"
 )
 
 func TestInvoke_QueryAccountList(t *testing.T) {
-	Init()
-	response, e := ChannelQuery("queryAccountList", [][]byte{})
+	blockchain.Init()
+	response, e := blockchain.ChannelQuery("queryAccountList", [][]byte{})
 	if e != nil {
 		fmt.Println(e.Error())
 		t.FailNow()
