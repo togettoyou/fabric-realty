ab7ece2fad7e15ffca49433d7c08ed34667520e4
togettoyou <zoujh99@qq.com>
2021-04-26 23:05:03 +0800

    一键部署，应用

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,15 @@
-FROM golang:1.14
+FROM node:12.16.3-alpine AS builder_vue
+COPY vue /root/vue
+WORKDIR /root/vue
+RUN yarn config set registry http://registry.npm.taobao.org \
+  && yarn config set sass-binary-site http://npm.taobao.org/mirrors/node-sass \
+  && yarn install
+RUN yarn build:prod
 
+FROM golang:1.14
 ENV GO111MODULE=on
 ENV GOPROXY=https://goproxy.cn,direct
-
-RUN apt update \
-    && apt-get -y install nodejs npm
-
-COPY application /root/blockchain-real-estate/application
-
-WORKDIR /root/blockchain-real-estate/application/vue
-RUN npm config set registry "https://registry.npm.taobao.org/" \
-  && npm i node-sass --sass_binary_site="https://npm.taobao.org/mirrors/node-sass/" \
-  && npm install
-RUN npm run build:prod
-
-WORKDIR /root/blockchain-real-estate/application
+COPY application /root/application
+COPY --from=builder_vue /root/vue/dist /root/application/dist
+WORKDIR /root/application
 RUN go build -o "app" .
--- start.sh
+++ start.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-echo "删除旧容器"
+echo "一、删除旧容器"
 docker rm -f blockchain-real-estate-application
 
-echo "开始打包编译"
-docker build -t togettoyou/blockchain-real-estate:1.0 .
+echo "二、开始打包编译"
+docker build -t togettoyou/blockchain-real-estate:v1 .
 
-echo "运行容器"
-docker run -it -d --name blockchain-real-estate-application togettoyou/blockchain-real-estate:1.0
+echo "三、运行编译容器"
+docker run -it -d --name blockchain-real-estate-application togettoyou/blockchain-real-estate:v1
 
-echo "拷贝容器中编译后的文件"
-docker cp blockchain-real-estate-application:/root/blockchain-real-estate/application/app ./application/
-docker cp blockchain-real-estate-application:/root/blockchain-real-estate/application/vue/dist ./application/dist
+echo "四、拷贝容器中编译后的文件"
+docker cp blockchain-real-estate-application:/root/application/app ./application/
+docker cp blockchain-real-estate-application:/root/application/dist ./application/dist
 
-echo "运行"
-./application/app
+if [[ `uname` == 'Darwin' ]]; then
+    echo "Mac OS 请自行在 application 下 go run main.go 或 go build"
+fi
+if [[ `uname` == 'Linux' ]]; then
+    echo "五、运行"
+    ./application/app
+fi
