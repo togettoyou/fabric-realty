b27bea11688bd64ccc309841d30ef605591c801d
zoujh <728600405@qq.com>
2021-04-27 09:27:57 +0800

    一键部署

--- application/main.go
+++ application/main.go
@@ -20,7 +20,7 @@ func init() {
 // @title 基于区块链技术的房地产交易系统api文档
 // @version 1.0
 // @description 基于区块链技术的房地产交易系统api文档
-// @contact.name 夜央 Oh oh oh oh oh oh
+// @contact.name togettoyou
 // @contact.email zoujh99@qq.com
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
--- application/start.sh
+++ application/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "运行单元测试，检查区块链网络是否连接成功"
+go test sdk_test.go
+echo "开始准备启动应用"
+sleep 2
+
 echo "一、清理环境、删除旧容器"
 rm -rf app
 docker rm -f blockchain-real-estate-application
@@ -17,6 +22,6 @@ echo "三、运行编译容器"
 docker run -it -d --name blockchain-real-estate-application togettoyou/blockchain-real-estate:v1
 
 echo "四、拷贝容器中编译后的二进制可执行文件"
-docker cp blockchain-real-estate-application:/root/application/app ./application/
+docker cp blockchain-real-estate-application:/root/application/app ./
 sudo chmod +x ./app
 ./app
