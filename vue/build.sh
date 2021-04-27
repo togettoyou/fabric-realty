ef83c4bc14e55a804cff1b13a41fcb572fe6db5d
zoujh <728600405@qq.com>
2021-04-27 14:12:03 +0800

    部署文档

--- vue/build.sh
+++ vue/build.sh
@@ -10,5 +10,5 @@ docker build -t togettoyou/blockchain-real-estate-vue:v1 .
 echo "三、运行编译容器"
 docker run -it -d --name blockchain-real-estate-vue togettoyou/blockchain-real-estate-vue:v1
 
-echo "四、拷贝容器中编译后的文件并运行"
+echo "四、拷贝容器中编译后的dist资源并放到application目录下"
 docker cp blockchain-real-estate-vue:/root/vue/dist ./../application/dist
