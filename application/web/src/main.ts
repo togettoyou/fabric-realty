ed31a0d0d4486790dd8f07ca3fe26c411a9d1bcb
Junhao Zou <zoujh99@qq.com>
2025-01-18 18:07:21 +0800

    前端实现

--- application/web/src/main.ts
+++ application/web/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
 
 import './style.css'
 
