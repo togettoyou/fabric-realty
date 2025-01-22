6ae7bcedf487d1079854abcd053ee956b61cd666
togettoyou <zoujh99@qq.com>
2025-01-22 11:39:15 +0800

    前端调试

--- application/web/vite.config.ts
+++ application/web/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8888',
         changeOrigin: true,
       },
     },
